95e10a542dd933a1812881dfe89d3acd16b2a940
fallpaint <126129642+fallpaint@users.noreply.github.com>
2023-03-13 22:56:21 +0800

    Update Main.java

--- src/main/java/prog2/exercise4/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise4/flight/booking/system/Main.java
@@ -1,46 +1,116 @@
-package exercise3.flight.booking.system;
-
-
-import java.time.LocalDate;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        
-        Scanner reader = new Scanner(System.in);
-        
-        System.out.println("Please enter your full name:");
-        String name = reader.nextLine();
+public class Main {
+    public static void main(String[] args) {
+        String flightCompany="Flights-of-Fancy";
+        Random random = new Random();
+        int number = random.nextInt(90000)+10000;
+        int a,b,c;
+        a = (int) (Math.random() * 26 + 65);
+        b = (int) (Math.random() * 26 + 65);
+        c= (int) (Math.random() * 26 + 65);
+        String flightID =(char)i+""+(char)j+""+number+""+(char)k;
+        System.out.println("Please enter your full name here");
 
-        System.out.println("Now, please enter your departure date as YYYY-MM-DD:");
-        String sdepart = reader.nextLine();
-        LocalDate depart = LocalDate.parse(sdepart);
+        Scanner getpassengerFullName = new Scanner(System.in);
 
-        System.out.println("Now, please enter your return date as YYYY-MM-DD:");
-        String sreturn = reader.nextLine();
-        LocalDate returnDate = LocalDate.parse(sreturn);
-        
-        System.out.println("How many children as passengers. Enter 0 if none: ");
-        int children = reader.nextInt();
+        String passengerFullName = getpassengerFullName.nextLine();
 
-        System.out.println("How many adults as passengers: ");
-        int adults = reader.nextInt();
+        String tripSource ="NANJING";
 
-        FlightBooking flightbooking = new FlightBooking(name, depart, returnDate, children, adults);
+        String sourceAirport ="NANJING LUKOU INTERNATIONAL AIRPORT";
 
-        flightbooking.setTotalPassengers(children, adults);
-        flightbooking.setFlightID(name);
-        flightbooking.setTicketNumber(name);
-        flightbooking.setDepartingDate(depart);
-        flightbooking.setReturnDate(returnDate);
-        flightbooking.setDepartingTicketPrice(children, adults);
-        flightbooking.setReturnTicketPrice(children, adults);
-        flightbooking.setTotalTicketPrice();
-    
-        System.out.println(flightbooking);
-        
-        reader.close();
+        String tripDestination ="OULU";
+
+        String destinationAirport ="OULU AIRPORT";
+
+        System.out.println("Please enter your departureyear here");
+
+        Scanner getdepartureyear = new Scanner(System.in);
+
+        Integer departureyear = getdepartureyear.nextInt();
+
+        if(departureyear<1000||departureyear>2100){
+
+            System.out.println("Error!");
+
+            System.exit(0);
+        }
+        System.out.println("Please enter your departuremonth here");
+        Scanner getdeparturemonth = new Scanner(System.in);
+        Integer departuremonth = getdeparturemonth.nextInt();
+        if(departuremonth<1||departuremonth>12){
+
+            System.out.println("Error!");
+
+            System.exit(0);
+        }
+        System.out.println("Please enter your departureday here");
+        Scanner getdepartureday = new Scanner(System.in);
+        Integer departureday = getdepartureday.nextInt();
+        if(departureday<1||departureday>31){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        String departureDate=departureyear+"-"+departuremonth+"-"+departureday;
+        System.out.println("Please enter your returnyear here");
+        Scanner getreturnyear = new Scanner(System.in);
+        Integer returnyear = getreturnyear.nextInt();
+        if(returnyear<departureyear){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        System.out.println("Please enter your returnmonth here");
+        Scanner getreturnmonth = new Scanner(System.in);
+        Integer returnmonth = getreturnmonth.nextInt();
+        if(returnyear.equals(departureyear)&&returnmonth<departuremonth){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        if(returnmonth<1||returnmonth>12){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        System.out.println("Please enter your returnday here");
+        Scanner getreturnday = new Scanner(System.in);
+        Integer returnday = getreturnday.nextInt();
+        if(departureyear.equals(returnyear)&&returnmonth.equals(departuremonth)){
+            if(returnday<departureday) {
+                System.out.println("Error!");
+                System.exit(0);
+            }
+        }
+        if(returnday<1||returnday>31){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        String returnDate=returnyear+"-"+returnmonth+"-"+returnday;
+        //System.out.println(returnDate);
+        System.out.println("Please enter the number of children accompanying you");
+        Scanner getchildPassengers = new Scanner(System.in);
+        Integer childPassengers = getreturnday.nextInt();
+        if(childPassengers!=0){
+            System.out.println("Error!");
+            System.exit(0);
+        }
+        System.out.println("Please enter the number of adult passengers");
+        Scanner getadultPassengers = new Scanner(System.in);
+        Integer adultPassengers = getreturnday.nextInt();
+        int totalPassengers = childPassengers + adultPassengers;
+        int departingTicketPrice = 150;
+        int returnTicketPrice = 100;
+        int totalTicketPrice = departingTicketPrice + returnTicketPrice;
+        int getticketnumber = random.nextInt(90000)+10000;
+        int a = (int) (Math.random() * 26 + 65);
+        String ticketNumber =(char)a+""+getticketnumber;
+        System.out.println("Dear " + passengerFullName + ".Thank you for booking your flight with "+flightCompany +".");
+        System.out.println("Following are the details of your booking and the trip:");
+        System.out.println("Ticket Number: "+ticketNumber);
+        System.out.println("From " + tripSource+ " to " + tripDestination);
+        System.out.println("Date of departure: "+ departureDate);
+        System.out.println("Date of return: " + returnDate);
+        System.out.println("Total passengers: "+ totalPassengers);
+        System.out.println("Total ticket price in Euros: " + totalTicketPrice);
     }
 }
